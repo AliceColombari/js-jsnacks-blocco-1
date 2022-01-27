0df2af67af68d9d2d17db5ac2a84aa3b4812bec8
AliceColombari <colombari.23@gmail.com>
2022-01-27 18:32:37 +0100

    calcola il cubo di tutti i numeri precedenti a quello digitato

--- jsnack2/main.js
+++ jsnack2/main.js
@@ -1,5 +1,8 @@
 // Stampa il cubo dei primi N numeri, dove N è un numero indicato dall’utente.
 
+
+// calcola il cubo del numero digitato dall'utente
+/*
 let numero = parseInt(prompt('Inserisci un numero!'));
 
 let i = 0;
@@ -12,3 +15,23 @@ while (i <= numero) {
 
 let cubo = document.querySelector('.cubo');
 cubo.innerHTML = "Il cubo di " + numero + " è: " + (Math.pow(numero, 3));
+*/
+
+
+
+// calcola il cubo di tutti i numeri precedenti a quello digitato
+let numero = parseInt(prompt('Inserisci un numero!'));
+// primiNumeri farà si che venga calcolato il cubo di tutti i numeri precedenti a quello digitato
+let primiNumeri = 1;
+let cubo = 0;
+
+while (primiNumeri <= numero) {
+
+    cubo = primiNumeri**3;
+    console.log("Il cubo di " + primiNumeri + " è: " + cubo)
+
+    primiNumeri++;
+}
+
+let result = document.querySelector('.cubo');
+result.innerHTML = "Il cubo di " + numero + " è: " + cubo;
